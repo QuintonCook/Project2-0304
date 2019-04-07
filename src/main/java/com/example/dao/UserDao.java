28743f5658222ee6867f791cf32f617f80f866be
meme7 <manlopez77@gmail.com>
2019-04-07 14:19:51 -0500

    Adding java Bean and Dao files

---
+++ src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,58 @@
+package com.example.dao;
+
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.example.model.User;
+import com.example.model.User;
+import com.example.util.HibernateUtil;
+
+public class UserDao {
+	
+	public UserDao() {
+	}
+	
+	public void insert(User myUser) {
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = ses.beginTransaction();
+		
+		ses.save(myUser);
+		tx.commit();
+	}
+	
+	public User selectByCred(String username) {
+		Session ses = HibernateUtil.getSession();
+		
+		User myUser = ses.get(User.class, username);
+		
+		return myUser;
+	}
+	
+	public List<User> searchByFirstLast(String firstname, String lastname){
+		Session ses = HibernateUtil.getSession();
+		
+		Query<User> query = ses.createQuery("from User where firstname = :firstname and lastname = :lastname");
+		query.setParameter("firstname", firstname);
+		query.setParameter("lastname", lastname);
+		
+		List<User> list = query.list();
+		return list;
+	}
+	
+	public void updateUserProfile(User us) {
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = ses.beginTransaction();
+		
+		ses.update(us);
+		tx.commit();
+	}
+	
+	public List<User> selectAll(){
+		Session ses = HibernateUtil.getSession();
+		List<User> userList = ses.createCriteria(User.class).list();
+		return userList;
+	}
+
+}
---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,127 @@
+package com.example.model;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USERS")
+public class User {
+	
+	@Column(name="firstname", nullable=false)
+	private String firstname;
+	
+	@Column(name="lastname", nullable=false)
+	private String lastname;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+	
+	@Id
+	@Column(name="email", nullable=false)
+	private String email;
+	
+	@OneToMany(mappedBy="key", fetch=FetchType.EAGER)
+	private List<Post> postList;
+	
+	public User() {
+		
+	}
+
+	public User(String firstname, String lastname, String password, String email, List<Post> postList) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.password = password;
+		this.email = email;
+		this.postList = postList;
+	}
+
+	public User(String firstname, String lastname, List<Post> postList) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.postList = postList;
+	}
+
+	public User(String password, String email) {
+		super();
+		this.password = password;
+		this.email = email;
+	}
+
+	public User(List<Post> postList, String password, String email) {
+		super();
+		this.password = password;
+		this.email = email;
+		this.postList = postList;
+	}
+
+	public User(String firstname, String lastname, String password, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.password = password;
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Post> getPostList() {
+		return postList;
+	}
+
+	public void setPostList(List<Post> postList) {
+		this.postList = postList;
+	}
+
+	@Override
+	public String toString() {
+		return "User [firstname=" + firstname + ", lastname=" + lastname + ", password=" + password + ", email=" + email
+				+ "]";
+	}
+	
+	
+	
+
+}
